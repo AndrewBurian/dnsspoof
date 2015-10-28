6cb9bb5094dce9d8d80fa824f75be85a7e36bfcf
Andrew Burian <andrew@burian.ca>
2015-10-28 16:13:17 -0700

    Minor tweaks to main prototype
    Checks for root
    Runs spoof on nic 'wlp3s0'

--- core.go
+++ core.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	spoof()
+	if os.Geteuid() != 0 {
+		fmt.Println("dnsspoof requires root!")
+		return
+	}
+
+	fmt.Println("Running spoofer")
+
+	spoof("wlp3s0")
 }
